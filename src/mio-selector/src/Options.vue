29be50d468f7dbd2d64ba4fbdd6bf3bb9973187e
Mimiokish <Business.Jiahe.Lin@gmail.com>
2024-07-16 18:29:34 +0800

    update: Selector

---
+++ src/mio-selector/src/Options.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "mio-options",
+    props: {
+        options: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="mio-options-container">
+        <template v-for="option in options">
+            <div>{{ option.label }}</div>
+        </template>
+    </div>
+</template>
--- src/mio-selector/src/Selector.vue
+++ src/mio-selector/src/Selector.vue
@@ -1,22 +1,46 @@
 <script>
+import Options from "./Options.vue";
 import Utils from "../../utils";
 
 export default {
     name: "mio-selector",
-    props: {},
+    components: [
+        Options
+    ],
+    props: {
+        modelValue: {
+            type: [String, Number, null],
+            default: null
+        },
+        options: {
+            type: Array,
+            default: () => []
+        }
+    },
     data() {
         return {
             UUID: Utils.GenerateUUID()
         };
     },
-    mounted() {
-        console.log("Selector mounted");
+    watch: {
+        list: {
+            immediate: true
+        }
     }
 };
 </script>
 
 <template>
-    <div :id="'MiO-UI-Selector-' + UUID" class="mio-ui-selector-container">Selector</div>
+    <div :id="'MiO-UI-Selector-' + UUID" class="mio-ui-selector-container">
+        <div v-if="options.length <= 0" class="selector-no-data">暂无数据</div>
+        <template v-else>
+            <template v-for="option in options">
+                <div v-if="option.value === modelValue" class="selector-value">{{ option.label }}</div>
+            </template>
+            <div class="selector-icon"></div>
+            <Options :id="'MiO-UI-Options-' + UUID" :options="options" />
+        </template>
+    </div>
 </template>
 
 <style scoped></style>
